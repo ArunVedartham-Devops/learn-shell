dfc7b7525391f30c6c9561166e7dfec9b9668c71
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-03 19:15:57 +0530

    Variable declaration

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,5 +1,14 @@
 # syntax : var=data
+# user defined variable
 x=10
 
 # how to access : $var or ${var}
 echo x is $x
+
+# system special variable
+# $0, $1.. $n
+# $#, $*, $?
+
+# $? means Exit status os a command
+ls
+echo exit status - $?
