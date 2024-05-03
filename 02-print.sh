b92e7701e80e7d98663415a59934c0e3e193e82f
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-03 18:48:49 +0530

    frontend add headings with colour

--- 02-print.sh
+++ 02-print.sh
@@ -1,4 +1,4 @@
-echo hello world
+echo Hello World
 # syntax : echo -e "\e[COLmMESSAGE\e[0m"
 echo -e "\e[31Hello World\e[0m"
 echo -e "\e[32Hello World\e[0m"
@@ -6,3 +6,4 @@ echo -e "\e[33Hello World\e[0m"
 echo -e "\e[34Hello World\e[0m"
 echo -e "\e[35Hello World\e[0m"
 echo -e "\e[36Hello World\e[0m"
+
