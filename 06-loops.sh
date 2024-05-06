2704a935e40d50c8f6e4dbf28d21183ea1c5f372
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-06 11:04:13 +0530

    Loop conditions

---
+++ 06-loops.sh
@@ -0,0 +1,14 @@
+# while
+i=10
+while [ $i -gt 0 ]; do
+  echo Hello
+  i=$(($i-1))
+  Sleep 2
+  done
+
+  # for
+
+  For course in AWS AZURE DEVOPS ; do
+    echo Welcome to $course training
+    Sleep 2
+    done
