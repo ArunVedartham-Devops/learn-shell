868cf1e30dc6811c60037db8bf46a12d282cb66a
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-06 07:51:48 +0530

    Conditions test

---
+++ 05-conditions.sh
@@ -0,0 +1,15 @@
+a=10
+
+if [ $a -gt 5 ]; then
+   echo $a is greter than 5
+   fi
+
+   b=80
+
+   if [ $b -gt 75 ]; then
+      echo pass - distinction
+   elsif [ $marks -gt 60 ]; then
+      echo pass - first class
+   else
+        echo fail / second class
+  fi
